20618a4d53cd36799dc73a30277da79d3037e642
Oscar B <oscar.boys2@outlook.com>
2025-04-23 19:07:55 +1000

    feat: rename sentiment analysis routes to be more specific, and add CUDA availability check script

--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -1,13 +1,13 @@
 # __init__.py
 # This module imports and aggregates all the route modules for the API as a single package.
 
-from .analyze import router as analyze_router
+from .analyze_sentiment import router as sentiment_router
 
 # In the future, import other routes here
 # from .auth import router as auth_router
 
 # Aggregate all routes in one place
 routers = [
-    analyze_router,
+    sentiment_router,
     # Add more routers as you go
 ]
